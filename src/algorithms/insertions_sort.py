e43d1f03727825287e26a9ad8d9d29a4ba796d9f
alexguja <al.guja@outlook.com>
2024-08-05 01:13:57 +0200

    add insertion sort

---
+++ src/algorithms/insertions_sort.py
@@ -0,0 +1,43 @@
+from typing import List
+
+def insertion_sort(array):
+    """Sorts an array in place using insertion sort in O(n^2) time.
+
+    Args:
+        array (List): The array to be sorted.
+
+    Returns:
+        List: The sorted array.
+    
+    Example:
+        Given an array [5, 2, 4, 6, 1, 3]. 
+        
+        The algorithm will sort the array as follows:
+
+        Step 1: Swaps 5 and 2
+            [5, 2, 4, 6, 1, 3] => [2, 5, 4, 6, 1, 3]
+        
+        Step 2: Swaps 5 and 4
+            [2, 5, 4, 6, 1, 3] => [2, 4, 5, 6, 1, 3]
+        
+        Step 3: No swaps
+            [2, 4, 5, 6, 1, 3] => [2, 4, 5, 6, 1, 3]
+        
+        Step 4: Move 1 to the front and shift the rest
+            [2, 4, 5, 6, 1, 3] => [1, 2, 4, 5, 6, 3]
+        
+        Step 5: Move 3 to the correct position and shift the rest
+            [1, 2, 4, 5, 6, 3] => [1, 2, 3, 4, 5, 6]
+
+    """
+    for i in range(1, len(array)):
+        key = array[i] 
+        j = i -1
+        while j > -1 and array[j] > key:
+            array[j + 1] = array[j]
+            j -= 1
+        array[j + 1] = key 
+
+    return array
+
+        
