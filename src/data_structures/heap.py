10b4ca25705159551b821fa30b46a7d3305bdb4b
alexguja <al.guja@outlook.com>
2024-08-05 13:43:02 +0200

    update docstrings

--- src/data_structures/heap.py
+++ src/data_structures/heap.py
@@ -11,21 +11,8 @@ class MaxHeap:
 
     def right(self, i) -> int:
         return 2 * i + 2
-    
-    def __swap(self, i, j):
-        self.data[i], self.data[j] = self.data[j], self.data[i]
-
-    def __bubble_up(self, i):
-        while i > 0 and self.data[self.parent(i)] < self.data[i]:
-            self.__swap(i, self.parent(i))
-            i = self.parent(i)
 
     def max_heapify(self, i):
-        """Maintains the heap invariant for a max heap in O(lg n) time.
-
-        Args:
-            i (int): The index of the heap element.
-        """
         left = self.left(i)
         right = self.right(i)
         largest = i
@@ -33,33 +20,15 @@ class MaxHeap:
         if left < self.heap_size and self.data[left] > self.data[i]:
             largest = left
 
-        if right < self.heap_size and self.data[right] > self.data[largest]:
+        if right <= self.heap_size and self.data[right] > self.data[largest]:
             largest = right
 
         if largest != i:
             self.__swap(i, largest)
-            self.max_heapify(largest)
-
-    def max_heapify_iterative(self, i):
-        """Iterative version of max_heapify"""
-
-        while i < self.heap_size:
-            left = self.left(i)
-            right = self.right(i)
-            largest = i
-
-            if left <= self.heap_size and self.data[left] > self.data[i]:
-                largest = left
-
-            if right <= self.heap_size and self.data[right] > self.data[largest]:
-                largest = right
-            
-            if largest != i:
-                self.__swap(i, largest)
-                i = largest
+            i = largest
 
     def build_max_heap(self, array):
-        """Builds a max heap from a list of elements in O(n) time."""
+        """Builds a max heap from a list of elements."""
 
         self.data = array
         self.heap_size = len(array)
@@ -67,16 +36,15 @@ class MaxHeap:
             self.max_heapify(i)
 
     def insert(self, key):
-        """Inserts a new element into the heap in O(lg n) time."""
+        """Inserts a new element into the heap."""
 
         self.heap_size += 1
         self.data.append(key)
         i = self.heap_size - 1
         self.__bubble_up(i)
 
-
     def increase_key(self, i, key):
-        """Increases the value of element at index `i` to `key` in O(lg n) time."""
+        """Increases the value of element at index `i` to `key`."""
 
         if key < self.data[i]:
             raise ValueError("New key is smaller than current key")
@@ -84,38 +52,38 @@ class MaxHeap:
         self.__bubble_up(i)
 
     def delete(self, i):
-        """Deletes the element at index i in O(lg n) time."""
-
-        self.increase_key(i, float("inf")) # will bubble up the key at i to the root
-        self.__swap(0, self.heap_size - 1) # swap the root with the last element
-        self.data.pop() # optionally remove the last element
-        self.heap_size -= 1 
-
-        # Restore the heap invariant 
+        """Deletes the element at index `i`."""
+        self.increase_key(i, float("inf"))
+        self.data[0] = self.data[self.heap_size - 1]
+        self.data.pop()
+        self.heap_size -= 1
         self.max_heapify(0)
-    
+
     def max_element(self):
-        """Returns the maximum element from the heap in O(1) time."""
+        """Returns the maximum element in the heap."""
         return self.data[0]
 
     def extract_max(self):
-        """Extracts the maximum element from the heap in O(lg n) time."""
+        """Extracts and returns the maximum element from the heap."""
+
         if self.heap_size < 1:
             raise IndexError("Heap underflow")
 
         max_element = self.data[0]
         self.data[0] = self.data[self.heap_size - 1]
         self.heap_size -= 1
-        # self.data.pop() # optionally remove the last element
+        self.data.pop()
         self.max_heapify(0)
 
         return max_element
 
     def heapsort(self):
-        """Sorts a heap in place in O(n lg n) time."""
+        """Sorts the array using heap sort."""
         self.build_max_heap(self.data)
         for i in range(len(self.data) - 1, 0, -1):
-            self.swap(0, i)
+            self.__swap(0, i)
             self.heap_size -= 1
             self.max_heapify(0)
-        return self.data
+
+    def __swap(self, i, j):
+        self.data[i], self.data[j] = self.data[j], self.data[i]
