663cea3a355ee1ffa3929c4a829dc13d93caf99a
alexguja <al.guja@outlook.com>
2024-08-06 13:05:22 +0200

    update heap

--- src/data_structures/heap.py
+++ src/data_structures/heap.py
@@ -2,7 +2,7 @@ INFNITY = float("inf")
 
 class MaxHeap:
     def __init__(self):
-        self.data = []
+        self.heap = []
         self.heap_size = 0
 
     def parent(self, i) -> int:
@@ -25,10 +25,10 @@ class MaxHeap:
         right = self.right(i)
         largest = i
 
-        if left < self.heap_size and self.data[left] > self.data[i]:
+        if left < self.heap_size and self.heap[left] > self.heap[i]:
             largest = left
 
-        if right <= self.heap_size and self.data[right] > self.data[largest]:
+        if right < self.heap_size and self.heap[right] > self.heap[largest]:
             largest = right
 
         if largest != i:
@@ -42,10 +42,10 @@ class MaxHeap:
             right = self.right(i)
             largest = i
 
-            if left < self.heap_size and self.data[left] > self.data[i]:
+            if left < self.heap_size and self.heap[left] > self.heap[i]:
                 largest = left
 
-            if right <= self.heap_size and self.data[right] > self.data[largest]:
+            if right <= self.heap_size and self.heap[right] > self.heap[largest]:
                 largest = right
 
             if largest != i:
@@ -55,37 +55,37 @@ class MaxHeap:
     def build_max_heap(self, array):
         """Builds a max heap from a list of elements."""
 
-        self.data = array
-        for i in range(len(self.data) // 2 - 1, -1, -1):
+        self.heap = array
+        for i in range(len(self.heap) // 2 - 1, -1, -1):
             self.max_heapify(i)
 
     def insert(self, key):
         """Inserts a new element into the heap."""
 
         self.heap_size += 1
-        self.data.append(key)
+        self.heap.append(key)
         self.__bubble_up(self.heap_size - 1)
 
     def increase_key(self, i, key):
         """Increases the value of element at index i to key."""
 
-        if key < self.data[i]:
+        if key < self.heap[i]:
             raise ValueError("New key is smaller than current key")
-        self.data[i] = key
+        self.heap[i] = key
         self.__bubble_up(i) # Time Complexity: O(lg n)
 
     def delete(self, i):
         """Deletes the element at index i."""
         
         self.increase_key(i, INFNITY)
-        self.data[0] = self.data[self.heap_size - 1]
-        self.data.pop() # Optional step
+        self.heap[0] = self.heap[self.heap_size - 1]
+        self.heap.pop() # Optional step
         self.heap_size -= 1
         self.max_heapify(0) # Time Complexity: O(lg n)
 
     def max_element(self):
         """Returns the maximum element in the heap."""
-        return self.data[0] # Time Complexity: O(1)
+        return self.heap[0] # Time Complexity: O(1)
 
     def extract_max(self):
         """Extracts and returns the maximum element from the heap."""
@@ -94,10 +94,10 @@ class MaxHeap:
         if self.heap_size < 1:
             raise IndexError("Heap underflow")
 
-        max_element = self.data[0]
-        self.data[0] = self.data[self.heap_size - 1]
+        max_element = self.heap[0]
+        self.heap[0] = self.heap[self.heap_size - 1]
         self.heap_size -= 1
-        self.data.pop()
+        self.heap.pop()
         self.max_heapify(0) # O(lg n)
 
         return max_element
@@ -111,17 +111,17 @@ class MaxHeap:
         """
 
         # Time complexity: O(n lg n)
-        self.build_max_heap(self.data) # O(n)
-        for i in range(len(self.data) - 1, 0, -1):
+        self.build_max_heap(self.heap) # O(n)
+        for i in range(len(self.heap) - 1, 0, -1):
             self.__swap(0, i)
             self.heap_size -= 1
             self.max_heapify(0) # O(lg n)
 
     def __swap(self, i, j):
-        self.data[i], self.data[j] = self.data[j], self.data[i]
+        self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
 
     def __bubble_up(self, i):
-        while i > 0 and self.data[self.parent(i)] < self.data[i]:
+        while i > 0 and self.heap[self.parent(i)] < self.heap[i]:
             self.__swap(i, self.parent(i))
             i = self.parent(i)
     
