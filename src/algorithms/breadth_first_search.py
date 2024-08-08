9a1a6a860751b4113fd3925f33714454b4e24e73
alexguja <al.guja@outlook.com>
2024-08-09 00:33:17 +0200

    add dfs and bfs

---
+++ src/algorithms/breadth_first_search.py
@@ -0,0 +1,17 @@
+from data_structures.queue import Queue
+
+def bfs(graph, start_vertex):
+    visited = set()
+    queue = Queue()
+
+    queue.enqueue(start_vertex)
+    visited.add(start_vertex)
+
+    while queue:
+        current_vertex = queue.dequeue()
+        for neighbour in graph[current_vertex]:
+            if neighbour not in visited:
+                visited.add(neighbour)
+                queue.enqueue(neighbour)
+    
+    return visited
---
+++ src/algorithms/depth_first_search.py
@@ -0,0 +1,18 @@
+def dfs(graph):
+    parent_vertices = {}
+    visited = set()
+
+    for vertex in graph:
+        if vertex not in visited:
+            parent_vertices[vertex] = None
+            dfs_visit(graph, vertex, visited, parent_vertices)
+            
+    return parent_vertices
+
+
+def dfs_visit(graph, start_vertex, visited, parent_vertices):
+    visited.add(start_vertex)
+    for neighbour in graph[start_vertex]:
+        if neighbour not in visited:
+            parent_vertices[neighbour] = start_vertex
+            dfs_visit(graph, neighbour, visited, parent_vertices)
