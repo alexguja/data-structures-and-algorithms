e1db82b9741eb7cf9d3ee6fd2c491150276eb7cb
alexguja <al.guja@outlook.com>
2025-02-23 19:22:55 +0100

    add trie implementation

--- src/data_structures/trie.py
+++ src/data_structures/trie.py
@@ -9,10 +9,41 @@ class Trie:
         self.root = TrieNode()
 
     def insert(self, word: str) -> None:
-        pass
+        current = self.root
+
+        for char in word:
+            # Add new character to the children hashmap if it doesn't exist
+            if char not in current.children:
+                current.children[char] = TrieNode()
+
+            # Move to the next node
+            current = current.children[char]
+
+        # Mark the end of the entry
+        current.is_entry = True
 
     def find(self, word: str) -> bool:
-        pass
+        current = self.root
+
+        for char in word:
+            # Detect missing node and return False
+            if char not in current.children:
+                return False
+
+            # Move to the next node
+            current = current.children[char]
+
+        return current.is_entry  # Verify end of entry
 
     def starts_with(self, prefix: str) -> bool:
-        pass
+        current = self.root
+
+        for char in prefix:
+            # Detect missing node and return False
+            if char not in current.children:
+                return False
+
+            # Move to the next node
+            current = current.children[char]
+
+        return True
