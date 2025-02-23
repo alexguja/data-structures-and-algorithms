37f23c4bf69664218222b5f4677da47b9a02261e
alexguja <al.guja@outlook.com>
2025-02-23 19:28:50 +0100

    clean up

--- src/algorithms/binary_search.py
+++ src/algorithms/binary_search.py
@@ -20,29 +20,3 @@ def binary_search(array, target):
         else:
             return mid
     return -1
-
-
-def optimised_binary_search(array, target):
-    """Optimised version of the Binary Search algorithm
-    that requires a single value comparison.
-
-    Args:
-        array (List[int]): The sorted list of integers in which to search.
-        target (int): The value to search for in the array.
-
-    Returns:
-        int: The index of the target in the array if found, otherwise -1.
-    """
-    low = 0
-    high = len(array) - 1
-
-    while low <= high:
-        mid = (low + high) // 2
-        diff = target - array[mid]
-        if diff < 0:
-            high = mid - 1
-        elif diff > 0:
-            low = mid + 1
-        else:
-            return mid
-    return -1 
